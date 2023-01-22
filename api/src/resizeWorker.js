39fefa21821e206d7cc8308cf38bb7d4bbfe6370
Krists Gackis <kristsgackis4@gmail.com>
2023-01-22 18:40:26 +0200

    Completed module

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -1,6 +1,6 @@
 const gm = require('gm');
 const { parentPort } = require('worker_threads');
-const (parentPort, workerData) = require('worker_threads')
+const {parentPort, workerData} = require('worker_threads')
 
 gm(workerData.source)
     .resize(100,100)
